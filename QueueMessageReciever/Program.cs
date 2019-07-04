99cdf9796aa38cf7108ed053ab90026d2e8ece43
RajeevPandit <rajeev.pandit@itron.com>
2019-07-04 22:08:39 +0530

    QueueSampleTest

---
+++ QueueMessageReciever/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace QueueMessageReciever
+{
+    class Program
+    {
+        public const string connectionstring = "Endpoint=sb://techdiksha.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=WPzEzTSF4UG65nZW0rWsXlTIbokRMGy7UZtJf0CKSns=";
+        public const string queueName = "TechQueue";
+        static QueueClient queueClient;
+        
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            queueClient = new QueueClient(connectionstring, queueName);
+
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the MessageHandler Options in terms of exception handling, number of concurrent messages to deliver etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+                // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
+                AutoComplete = false
+            };
+
+            // Register the function that will process messages
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the queueClient is created in ReceiveMode.PeekLock mode (which is default).
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+
+            // Note: Use the cancellationToken passed as necessary to determine if the queueClient has already been closed.
+            // If queueClient has already been Closed, you may chose to not call CompleteAsync() or AbandonAsync() etc. calls 
+            // to avoid unnecessary exceptions.
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ QueueSample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+namespace QueueSample
+{
+    public class Program
+    {
+        public const string connectionstring = "Endpoint=sb://techdiksha.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=WPzEzTSF4UG65nZW0rWsXlTIbokRMGy7UZtJf0CKSns=";
+        public const string queueName = "TechQueue";
+        static QueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+
+        static async Task MainAsync()
+        {
+            queueClient = new QueueClient(connectionstring, queueName);
+            await SendMessageToQueue();
+            await queueClient.CloseAsync();
+        }
+
+        public async static Task SendMessageToQueue()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                // Create a new message to send to the queue.
+                string messageBody = $"Message {i}";
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                // Write the body of the message to the console.
+                Console.WriteLine($"Sending message: {messageBody}");
+
+                // Send the message to the queue.
+                await queueClient.SendAsync(message);
+            }
+        }
+
+    }
+}
